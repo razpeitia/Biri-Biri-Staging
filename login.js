92e979dcaae69968daa67b64a0eda9f5834b47d8
Lucas Barés <lucasbaresm@gmail.com>
2018-06-26 23:57:26 -0300

    eliminated webos.js

--- login.js
+++ login.js
@@ -13,7 +13,6 @@ const bot = new Discord.Client({autoReconnect: true, max_message_cache: 0});
 const startup = require("./startup.js");
 const reaction = require("./reaction.js");
 const meme = require("./meme.js");
-//const webos = require("./webos.js");
 const help = require("./help.js");
 const emotions = require("./emotions.js");
 const changelog = require("./changelog.js")
@@ -22,7 +21,6 @@ startup.startup(bot);
 emotions.emotions(bot);
 help.help(bot);
 meme.meme(bot);
-//webos.webos(bot);
 reaction.reaction(bot);
 changelog.changelog(bot);
 
