e90ea0787a5f0a83db378866f67203c84fce56de
Lucas Uriel Mazzini <lucasbaresm@gmail.com>
2018-07-20 16:46:31 -0300

    fixed everyting

--- core/message.js
+++ core/message.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const utils = require('../core/utils.js')
 
 const footer = [`© FAMFAMO ~ `, 'https://cdn.discordapp.com/emojis/411791637870542851.png']
-const color = utils.randomColors();
+const color = 0x2e2538;
 
 class BaseMessage extends Discord.RichEmbed {
   constructor() {
