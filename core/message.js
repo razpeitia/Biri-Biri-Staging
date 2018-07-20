cb335ba4b9179049ec0ab6ea9f133635c9b9548f
Lucas Uriel Mazzini <lucasbaresm@gmail.com>
2018-07-20 16:59:55 -0300

    Intento no romper nada kek

--- core/message.js
+++ core/message.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js')
-const utils = require('../core/utils.js')
 
-const color = 0x2e2538;
+const footer = ['© FAMFAMO ~ ', 'https://cdn.discordapp.com/emojis/411791637870542851.png']
+const color = 0xff0000
 
 class BaseMessage extends Discord.RichEmbed {
   constructor() {
     super()
     this.setColor(color)
-    this.setFooter(`© ${msg.guild.name} ~ `, 'https://cdn.discordapp.com/emojis/411791637870542851.png')
+    this.setFooter(...footer)
   }
 }
 
