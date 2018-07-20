b02806dab6aba48333ec795b7e22f7ea3eabb1f1
Lucas Uriel Mazzini <lucasbaresm@gmail.com>
2018-07-20 16:57:16 -0300

    Changed footer?

--- core/message.js
+++ core/message.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js')
 const utils = require('../core/utils.js')
 
-const footer = [`© FAMFAMO ~ `, 'https://cdn.discordapp.com/emojis/411791637870542851.png']
 const color = 0x2e2538;
 
 class BaseMessage extends Discord.RichEmbed {
   constructor() {
     super()
     this.setColor(color)
-    this.setFooter(...footer)
+    this.setFooter(`© ${msg.guild.name} ~ `, 'https://cdn.discordapp.com/emojis/411791637870542851.png')
   }
 }
 
