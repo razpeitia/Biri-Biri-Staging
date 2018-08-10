df57a94e159d2e6a264468172a172be66f33a6ce
Ricardo Azpeitia Pimentel <razpeitia@gmail.com>
2018-08-10 16:00:15 -0500

    Fix startup

--- startup.js
+++ startup.js
@@ -1,3 +1,21 @@
+const getDefaultChannel = async (guild) => {
+  // get "original" default channel
+  if(guild.channels.has(guild.id))
+    return guild.channels.get(guild.id)
+
+  // Check for a "general" channel, which is often default chat
+  if(guild.channels.exists("name", "general"))
+    return guild.channels.find("name", "general");
+  // Now we get into the heavy stuff: first channel in order where the bot can speak
+  // hold on to your hats!
+  return guild.channels
+   .filter(c => c.type === "text" &&
+     c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
+   .sort((a, b) => a.position - b.position ||
+     Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
+   .first();
+}
+
 exports.startup = function(bot){
 
 	bot.on('ready', () => {
@@ -9,9 +27,9 @@ exports.startup = function(bot){
 
 
 	bot.on('guildMemberAdd',(gm) => {
-	    gm.guild.channels
-	    .find('name','lobby')
-	    .send('Bienvenido '+'<@' + gm.id +  '>' + ' <:vohiyo:412474913883160577>, no olvides leer las <#402905862274023435> <:pacman:420980551105642516>');
+	    const channel = getDefaultChannel(member.guild)
+	    if(channel === undefined || channel === null) return
+	    channel.send('Bienvenido '+'<@' + gm.id +  '>' + ' <:vohiyo:412474913883160577>, no olvides leer las <#402905862274023435> <:pacman:420980551105642516>');
 	});
 
 	bot.on("disconnect", event => {
