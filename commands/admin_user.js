5e5e8e6a7932487466dfd76ef1109677afd53144
Lucas Uriel Mazzini <lucasbaresm@gmail.com>
2018-09-19 14:46:28 -0300

    Final check for admin_user

--- commands/admin_user.js
+++ commands/admin_user.js
@@ -21,7 +21,7 @@ exports.getCommands = (clients) => {
           return send(msg, 'Necesitas especificar un comando, pendejo')
         }
         // Validation for deactivate the command command
-        if(maybeCommand === 'comando') {
+        if(maybeCommand === 'usercomando') {
           return send(msg, 'No <:wanwan:403968696067948554>')
         }
 
@@ -31,28 +31,31 @@ exports.getCommands = (clients) => {
           return send(msg, `Comando \`${maybeCommand}\` no encontrado`)
         }
 
-        let maybeAction = msg.content.trim().split(/\s+/)[2]
-        
+        let maybeUser   = msg.content.trim().split(/\s+/)[3]
+
         if(!maybeUser){
           return send(msg,`Necesitas etiquetar a alguien, pendejo`)
         }
         
-        let maybeUser   = msg.content.trim().split(/\s+/)[3]
         let id = utils.removeExtraFromId(maybeUser);
         
         if(id === authorId){
           return send(msg,`No te puedes hacer esto a ti mismo, pendejo`)
         }
-
+        
         if(id != NaN){
-          return send(msg,`Algo salio mal, etiquetaste a alguien, pendejo?`)
+          return send(msg,`Algo salio mal. Etiquetaste a alguien, pendejo?`)
         }
+        
+        let maybeAction = msg.content.trim().split(/\s+/)[2]
 
         if(maybeAction !== 'activar' && maybeAction !== 'desactivar') {
           return send(msg, `Necesitas especificar una action 'activar' o 'desactivar', pendejo`)
         }
+
         let action = (maybeAction === 'activar') ? 'activado' : 'desactivado'
         action === 'activado' ? command.enableUser(msg) : command.disableUser(msg)
+        
         return send(msg, `Comando ${command.name} ${action} para el usuario ${maybeUser}`)
     }
     })
