085722b9fe8c62e772e7217aae19c404acce7173
Ricardo Azpeitia Pimentel <razpeitia@gmail.com>
2020-07-19 19:55:34 -0500

    Use send method instead of trying to get default channel

--- startup.js
+++ startup.js
@@ -1,21 +1,3 @@
-const getDefaultChannel = async (guild) => {
-  // get "original" default channel
-  if(guild.channels.has(guild.id))
-    return guild.channels.get(guild.id)
-
-  // Check for a "general" channel, which is often default chat
-  if(guild.channels.exists("name", "general"))
-    return guild.channels.find("name", "general");
-  // Now we get into the heavy stuff: first channel in order where the bot can speak
-  // hold on to your hats!
-  return guild.channels
-   .filter(c => c.type === "text" &&
-     c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-   .sort((a, b) => a.position - b.position ||
-     Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
-   .first();
-}
-
 exports.startup = function(bot){
 
 	bot.on('ready', () => {
@@ -73,11 +55,9 @@ exports.startup = function(bot){
 		}
 	});
 
-	bot.on('guildMemberAdd',(gm) => {
-	    const channel = getDefaultChannel(gm.guild)
-	    if(channel === undefined || channel === null) return
+	bot.on('guildMemberAdd', (gm) => {
       try {
-	     channel.send('Bienvenido '+'<@' + gm.id +  '>' + ' <:vohiyo:412474913883160577>');
+	     gm.send(`Bienvenido <@${gm.id}> <:vohiyo:412474913883160577>`);
       } catch(err) {
         console.log(channel)
         console.log(err)
@@ -85,7 +65,7 @@ exports.startup = function(bot){
 	});
 
 	bot.on("disconnect", event => {
-	  console.log("Disconnected: " + event.reason + " (" + event.code + ")");
+	  console.log(`Disconnected: ${event.reason} (${event.code})`);
 	});
 
 };
