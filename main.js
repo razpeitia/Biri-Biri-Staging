4af8349e172a34b244985f801131b9982675a406
Ricardo Azpeitia Pimentel <razpeitia@gmail.com>
2020-07-19 19:05:16 -0500

    Comment workaround because it is breaking in v12

--- main.js
+++ main.js
@@ -17,9 +17,11 @@ if(env === 'production') {
 
 /* Workarround for shards disconnections */
 
+/* We need to port workaround to v12 of discord.js
 setTimeout(() => {
   bot.ws.connection.triggerReady()
 },30000)
+*/
 
 /* End */
 
